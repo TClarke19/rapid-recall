39cfcd146135fbd8279550f115e5a9efe15738ff
Nick <nickq4504@gmail.com>
2023-11-12 13:22:59 -0330

    Test commit. pages for home and project dashboard created.

---
+++ rapid-recall-fe/front-end/src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+    //React code
+
+    return (
+        <div>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/projects">Projects</Link>
+            </nav>
+            <h1>Home page</h1>
+        </div>
+    )
+}
+
+export default Home
