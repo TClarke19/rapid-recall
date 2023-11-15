24c9454031f161c0b182819d19209df3b10b726c
Buildertoo <102919008+Buildertoo@users.noreply.github.com>
2023-11-15 16:39:59 -0330

    Added BS comps to existing comps

--- rapid-recall-fe/front-end/src/components/Navbar.js
+++ rapid-recall-fe/front-end/src/components/Navbar.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -14,13 +18,30 @@ const Navbar = () => {
         return null;  // Don't show the navbar on the login page
     }
 
+    {/* I added the bootstrap navbar. We can decide if we want to go with */}
+
     return (
-        <nav>
-            <Link to="/home">Home</Link> |
-            <Link to="/projects">View Projects</Link> |
-            <Link to="/project_dashboard">Project Dashboard</Link> |
-            <button onClick={handleLogout}>Logout</button> {/* Logout Button */}
-        </nav>
+        <>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="/home">Rapid [Recall]</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/home">Home</Nav.Link>
+                        <Nav.Link href="/projects">Projects</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <nav>
+                <Link to="/home">Home</Link> |
+                <Link to="/projects">View Projects</Link> |
+                <Link to="/project_dashboard">Project Dashboard</Link> |
+                <Button variant='danger' onClick={handleLogout}>Logout</Button> {/* Logout Button */}
+            </nav>
+        </>
     );
 };
 
--- rapid-recall-fe/front-end/src/components/Projects/Project_Dash.js
+++ rapid-recall-fe/front-end/src/components/Projects/Project_Dash.js
@@ -2,17 +2,19 @@
 
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
 
 
 const ProjectDash = () => {
     return (
        <div>
-            <nav>
-                {/* <Link to="/">Home</Link>*/}
-                {/* <Link to="/projects">Projects</Link>*/}
-                {/* <Link to="/logout">Logout</Link>*/}
-            </nav>
+            <Navbar />
+            {/* <nav>
+                { <Link to="/">Home</Link> }
+                { <Link to="/projects">Projects</Link> }
+                { <Link to="/logout">Logout</Link> }
+            </nav> */}
            <h1>Project Dashboard</h1>
             <Button variant="primary">Add Card</Button>{' '}
             <Button variant="danger">Delete Card</Button>{' '}
